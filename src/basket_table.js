fb72468bcd0de0c128c78c73258c069d1e0d03e3
Daniel Brad <danieltbrad@gmail.com>
2017-10-02 10:35:03 -0700

    Load initial baskets

---
+++ src/basket_table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BasketTable = (props) => {
+  const baskets = props.baskets
+  return (
+    <div className="panel panel-default">
+      <h4 className="text-center">Basket Table</h4>
+    </div>
+  )
+};
+
+export default BasketTable;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
+import BasketTable from './basket_table'
+
+const URL = process.env.REACT_APP_URL;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { baskets: [] };
+  };
+
+  componentDidMount() {
+		axios.get(`${URL}/baskets`).then(response => {
+			this.setState({ baskets: response.data });
+		});
+	};
+
+
   render() {
     return (
       <div>
         <h3 className="text-center">Empty React Application</h3>
+        <BasketTable baskets={this.state.baskets}/>
       </div>
     );
   }
-}
+
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
