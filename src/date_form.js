401421c5a59e04a8c45e12c47b747c9204a5bb9c
Daniel Brad <danieltbrad@gmail.com>
2017-10-06 22:35:08 -0700

    Add more date validations

--- src/date_form.js
+++ src/date_form.js
@@ -5,67 +5,78 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 class DateForm extends React.Component {
   constructor (props) {
-    super(props)
+    super(props);
     this.state = {
       startDate: moment(props.start_date),
       endDate: moment(props.end_date),
       unit: props.unit,
-      dateOrderError: "hidden",
-      datePresenceError: "hidden"
-    };
+      missingDates: false,
+      outOfOrder: false,
+      tooShortforMonth: false,
+      tooShortforWeek: false };
     this.setStart = this.setStart.bind(this);
     this.setEnd = this.setEnd.bind(this);
   }
 
-  setStart(date) {
-    if(this.validateDateOrder({ start: date })) {
-      this.setState({ startDate: date, dateOrderError: "hidden", datePresenceError: "hidden" });
+  correctUnitforRange(){
+    var s = this.state.startDate
+    var e = this.state.endDate
+    var diff = e.diff(s, 'days');
+    if ((diff < 15) && (this.state.unit !== "day")) {
+      this.setState({ tooShortforWeek: true, tooShortforMonth: false })
+    }
+    else if (diff < 30 && this.state.unit === "month") {
+      this.setState({ tooShortforMonth: true, tooShortforWeek: false })
     }
     else {
-      this.setState({ datePresenceError: "hidden", dateOrderError: "" });
+      this.setState({ tooShortforMonth: false, tooShortforWeek: false })
     }
   };
 
-  setEnd(date) {
-    if (this.validateDateOrder({end: date})) {
-      this.setState({ endDate: date, dateOrderError: "hidden", datePresenceError: "hidden" });
+
+  checkErrors() {
+    var stateBeginning = this.state.startDate ? this.state.startDate._d : null
+    var stateFinish = this.state.endDate ? this.state.endDate._d : null
+    if (stateBeginning === null || stateFinish === null) {
+      this.setState({ missingDates: true, outOfOrder: false });
+    }
+    else if (stateBeginning >= stateFinish ) {
+      this.setState({ outOfOrder: true, missingDates: false });
     }
     else {
-      this.setState({ datePresenceError: "hidden", dateOrderError: "" });
+      this.setState({missingDates: false, outOfOrder: false}, this.correctUnitforRange)
     }
+  }
 
+  setStart(date) {
+    this.setState({ startDate: date }, this.checkErrors);
+  };
+
+  setEnd(date) {
+    this.setState({ endDate: date }, this.checkErrors);
   };
 
   handleSelect(){
     var newUnit = this.refs.unitSelector.value
-    this.setState({unit: newUnit});
-  };
-
-  validateDateOrder(date) {
-    var stateBeginning = this.state.startDate ? this.state.startDate._d : null
-    var stateFinish = this.state.endDate ? this.state.endDate._d : null
-    var newBeginning = date["start"] ? date["start"]._d : null
-    var newFinish = date["end"] ? date["end"]._d : null
-    if ((stateFinish === null || newBeginning < stateFinish) && (newFinish === null || newFinish > stateBeginning)) {
-      return true
-    }
+    this.setState({unit: newUnit}, this.checkErrors);
   };
 
   submitIfDatesPresent(){
-    if (this.state.startDate && this.state.endDate) {
-      this.setState({ datePresenceError: "hidden" });
-      this.props.changeDate(this.state.startDate, this.state.endDate, this.state.unit)
+    if (this.state.missingDates === true || this.state.outOfOrder === true || this.state.tooShortforMonth === true || this.state.tooShortforWeek === true) {
+      alert("Fix your date range before submitting")
     }
     else {
-      this.setState({datePresenceError: "", dateOrderError: "hidden"});
+      this.props.changeDate(this.state.startDate, this.state.endDate, this.state.unit)
     }
   };
 
   render() {
       return (
         <div className="content row">
-          <h5 className={this.state.dateOrderError} style={{ paddingLeft: '15px', color: 'red'}}>End date cannot be before start date!</h5>
-          <h5 className={this.state.datePresenceError} style={{ paddingLeft: '15px', color: 'red'}}>Please enter a start and end date!</h5>
+          <h5 className={this.state.outOfOrder === true ? '' : 'hidden'} style={{ paddingLeft: '15px', color: 'red'}}>End date cannot be before start date!</h5>
+          <h5 className={this.state.missingDates === true ? '' : 'hidden'} style={{ paddingLeft: '15px', color: 'red'}}>Please enter both a start and end date!</h5>
+          <h5 className={this.state.tooShortforMonth === true ? '' : 'hidden'} style={{ paddingLeft: '15px', color: 'red'}}>Ranges under a month need to be in weeks or days</h5>
+          <h5 className={this.state.tooShortforWeek === true ? '' : 'hidden'} style={{ paddingLeft: '15px', color: 'red'}}>Ranges this short must be in days</h5>
           <div>
             <div className="col-xs-3" style={{ paddingRight: '15px' }}>
               <DatePicker
