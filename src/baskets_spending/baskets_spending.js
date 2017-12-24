27af5fd8a32e126afd6346cf111519512651af22
Daniel Brad <danieltbrad@gmail.com>
2017-12-23 21:45:16 -0800

    Refactor basket_spending

--- src/baskets_spending/baskets_spending.js
+++ src/baskets_spending/baskets_spending.js
@@ -1,55 +1,27 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import { Alert } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import PropTypes from 'prop-types';
 import TokenHelper from '../auth/token_helper';
-import SpendingHistoryView from '../shared_components/spending_history_view';
 import BasketsTable from './baskets_table';
 import BasketsSpendingChart from './baskets_spending_chart';
 import BasketsSpendingView from './baskets_spending_view';
 import BasketService from '../api/basket_service';
 
 class BasketsSpending extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartData: null,
-      currentPage: 1,
-      desc: true,
-      loaded: false,
-      newest_date: moment('2017-09-25'),
-      oldest_date: moment('2015-11-23'),
-      pageOfResources: [],
-      perPage: 10,
-      totalPages: 0,
-      unit: null,
-    };
-    this.loadTable = this.loadTable.bind(this);
-    this.loadChart = this.loadChart.bind(this);
-    this.loadChartAndTable = this.loadChartAndTable.bind(this);
+  state = { loaded: false }
+
+  componentDidMount = () => {
+    this.loadChartAndTable({ user_id: TokenHelper.userId('jwt') });
   }
 
-  componentDidMount() {
-    this.loadChartAndTable({ desc: true, user_id: TokenHelper.userId('jwt') });
-  }
-
-  loadChartAndTable(args) {
+  loadChartAndTable = (args) => {
     const newArgs = { ...args, ...{ desc: true, page: 1 } };
     this.loadTable(newArgs, this.loadChart(newArgs));
   }
 
-  async loadChart(args) {
+  loadChart = async (args) => {
     if (this.props.isAuthenticated()) {
-      const params = {
-        user_id: args.user_id || null,
-        newest_date: args.newest_date == null ? this.state.newest_date : args.newest_date,
-        oldest_date: args.oldest_date == null ? this.state.oldest_date : args.oldest_date,
-        unit: args.unit || null,
-        productId: this.state.productId || null,
-      };
-      const response = await BasketService.getChart(params);
+      const response = await BasketService.getChart(args);
       if (response.status === 400) {
         this.setState({ error: response.data.message[0] });
       } else {
@@ -63,35 +35,22 @@ class BasketsSpending extends Component {
     }
   }
 
-  async loadTable(args) {
+  loadTable = async ( { desc, page, user_id, category, newest_date = this.state.newest_date, oldest_date=this.state.oldest_date, per_page = 10 } ) => {
     if (this.props.isAuthenticated()) {
-      const direction = args.desc === true ? 'desc' : 'asc';
-      const category = args.category !== undefined ? args.category : null;
-      const params = {
-        user_id: args.user_id || null,
-        category,
-        direction: direction || null,
-        newest_date: args.newest_date == null ? this.state.newest_date : args.newest_date,
-        oldest_date: args.oldest_date == null ? this.state.oldest_date : args.oldest_date,
-        page: args.page || null,
-        per_page: args.per_page || this.state.perPage,
-        productId: this.state.productId || null,
-      };
-      const response = await BasketService.getBaskets(params);
+      const response = await BasketService.getBaskets({ user_id, category, newest_date, oldest_date, page, per_page, desc });
       if (response.status !== 200) {
         this.setState({ error: response.data.errors[0] });
       } else {
         TokenHelper.set('jwt', response.headers.jwt);
         this.setState(prevState => (
           {
-            currentPage: args.page || prevState.currentPage,
-            desc: args.desc,
+            currentPage: page || prevState.currentPage,
+            desc,
             loaded: true,
-            newest_date: args.newest_date || prevState.newest_date,
-            oldest_date: args.oldest_date || prevState.oldest_date,
+            newest_date: response.headers.newest_date || this.state.newest_date,
+            oldest_date: response.headers.oldest_date || this.state.oldest_date,
             pageOfResources: response.data,
-            perPage: args.per_page || prevState.perPage,
-            sortCategory: args.category,
+            sortCategory: category,
             totalPages: Math.ceil(response.headers.total / response.headers['per-page']) || prevState.totalPages,
           }));
       }
