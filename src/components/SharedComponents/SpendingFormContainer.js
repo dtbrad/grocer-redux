ffd94524bed9ebf2288c61fb90d630bd0ccc21be
Daniel Brad <danieltbrad@gmail.com>
2018-01-10 09:13:32 -0800

    Refactor spending form validations

--- src/components/SharedComponents/SpendingFormContainer.js
+++ src/components/SharedComponents/SpendingFormContainer.js
@@ -8,6 +8,8 @@ class SpendingFormContainer extends Component {
     oldestDate: moment(this.props.oldestDate),
     newestDate: moment(this.props.newestDate),
     unit: this.props.unit || 'month',
+    outOfOrder: null,
+    wrongUnit: null,
   };
 
   componentWillReceiveProps = (nextProps) => {
@@ -20,26 +22,65 @@ class SpendingFormContainer extends Component {
 
   setOldest = async (date) => {
     await this.setState({ oldestDate: date });
-    // this.checkErrors();
+    this.checkErrors();
   }
 
   setNewest = async (date) => {
     await this.setState({ newestDate: date });
-    // this.checkErrors();
+    this.checkErrors();
   }
 
    setUnit = async (dan) => {
      await this.setState({ unit: dan.target.value });
-     // this.checkErrors();
+     this.checkErrors();
+   }
+
+   checkErrors = () => {
+     if (this.state.oldestDate >= this.state.newestDate) {
+       this.setState({
+         outOfOrder: true,
+         wrongUnit: false,
+         errorMessage: 'End date cannot be before start date!',
+       });
+     } else {
+       this.setState({ outOfOrder: false, errorMessage: null, wrongUnit: false });
+       this.correctUnitforRange();
+     }
+   }
+
+   correctUnitforRange = async () => {
+     const diff = this.state.newestDate.diff(this.state.oldestDate, 'days');
+     if ((diff < 15) && (this.state.unit !== 'day')) {
+       await this.setState({
+         wrongUnit: true,
+         errorMessage: 'Ranges this short must be in days',
+       });
+     } else if (diff < 30 && this.state.unit === 'month') {
+       await this.setState({
+         wrongUnit: true,
+         errorMessage: 'Ranges under a month need to be in weeks or days',
+       });
+     } else {
+       await this.setState({ tooShortForMonth: false, tooShortForWeek: false });
+     }
+   }
+
+   loadAppropriateData = (args) => {
+     const sameDates = (DeepEqual(this.state.oldestDate, moment(this.props.oldestDate)) &&
+       DeepEqual(this.state.newestDate, moment(this.props.newestDate)));
+     if (sameDates && this.state.unit === this.props.unit) {
+       return
+     } else if (sameDates) {
+       this.props.loadChart(args);
+     }
+     this.props.loadSpendingTableAndChart(args);
    }
 
   submitForm = (f) => {
     f.preventDefault();
-    const args = { oldestDate: this.state.oldestDate._d, newestDate: this.state.newestDate._d, unit: this.state.unit, resourceName: this.props.resourceName, desc: true, page: 1, productId: this.props.productId, userId: this.props.userId }
-    if (DeepEqual(this.state.oldestDate, moment(this.props.oldestDate)) && DeepEqual(this.state.newestDate, moment(this.props.newestDate))) {
-      this.props.loadChart(args);
-    } else {
-      this.props.loadSpendingTableAndChart(args);
+    if (!this.state.outOfOrder && !this.state.wrongUnit) {
+      const args = { oldestDate: this.state.oldestDate._d, newestDate: this.state.newestDate._d, unit: this.state.unit, resourceName: this.props.resourceName, desc: true, page: 1, productId: this.props.productId, userId: this.props.userId }
+      this.loadAppropriateData(args);
     }
   }
 
@@ -53,6 +94,9 @@ class SpendingFormContainer extends Component {
         setNewest={this.setNewest}
         setUnit={this.setUnit}
         submitForm={this.submitForm}
+        outOfOrder={this.state.outOfOrder}
+        wrongUnit={this.state.wrongUnit}
+        errorMessage={this.state.errorMessage}
       />
     );
   }
--- src/components/SharedComponents/SpendingFormView.js
+++ src/components/SharedComponents/SpendingFormView.js
@@ -1,57 +1,74 @@
 import React from 'react';
-import { Col, Form, FormGroup, FormControl, Button, Row } from 'react-bootstrap';
+import { Alert, Col, Form, FormGroup, FormControl, Button, Row } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const SpendingFormView = props => (
+const SpendingFormView = (props) => {
+  const errorAlert = (props.outOfOrder === true || props.wrongUnit === true) ? <Alert bsStyle="danger">{ props.errorMessage }</Alert> : null;
 
-  <Row style={{ paddingBottom: '15px' }}>
-    <Form inline onSubmit={props.submitForm}>
-      <Col xs={3}>
-        <FormGroup controlId="oldestDate">
-          <DatePicker
-            placeholderText={props.oldestDate}
-            selected={props.oldestDate}
-            onChange={props.setOldest}
-            className="datepicker form-control"
-            showYearDropdown
-          />
-        </FormGroup>
-      </Col>
-      <Col xs={3} >
-        <FormGroup controlId="newestDate" >
-          <DatePicker
-            placeholderText={props.newestDate}
-            selected={props.newestDate}
-            onChange={props.setNewest}
-            className="datepicker form-control"
-            showYearDropdown
-          />
-        </FormGroup>
-      </Col>
-      <Col xs={3}>
-        <FormGroup controlId="unit">
-          <FormControl
-            componentClass="select"
-            placeholder="Unit"
-            defaultValue="Month"
-            selected={props.unit}
-            onChange={props.setUnit}
-          >
-            <option value="month">Month</option>
-            <option value="week">Week</option>
-            <option value="day">Day</option>
-          </FormControl>
-        </FormGroup>
-      </Col>
-      <Col xs={3} >
-        <Button block type="submit">
-          Submit
-        </Button>
-      </Col>
-    </Form>
-    <br />
-  </Row>
-);
+  return (
+    <div>
+      <Row style={{ paddingBottom: '15px' }}>
+        <Form inline onSubmit={props.submitForm}>
+          <Col xs={3}>
+            <FormGroup
+              controlId={props.dateError}
+              validationState={props.outOfOrder === true ? 'error' : 'success'}
+            >
+              <DatePicker
+                readOnly
+                placeholderText={props.oldestDate}
+                selected={props.oldestDate}
+                onChange={props.setOldest}
+                className="datepicker form-control"
+                showYearDropdown
+              />
+            </FormGroup>
+          </Col>
+          <Col xs={3} >
+            <FormGroup
+              controlId="newestDate"
+              validationState={props.outOfOrder === true ? 'error' : 'success'}
+            >
+              <DatePicker
+                readOnly
+                placeholderText={props.newestDate}
+                selected={props.newestDate}
+                onChange={props.setNewest}
+                className="datepicker form-control"
+                showYearDropdown
+              />
+            </FormGroup>
+          </Col>
+          <Col xs={3}>
+            <FormGroup
+              controlId="unit"
+              validationState={props.wrongUnit === true ? 'error' : 'success'}
+            >
+              <FormControl
+                componentClass="select"
+                placeholder="Unit"
+                defaultValue="Month"
+                selected={props.unit}
+                onChange={props.setUnit}
+              >
+                <option value="month">Month</option>
+                <option value="week">Week</option>
+                <option value="day">Day</option>
+              </FormControl>
+            </FormGroup>
+          </Col>
+          <Col xs={3} >
+            <Button block type="submit">
+              Submit
+            </Button>
+          </Col>
+        </Form>
+        <br />
+      </Row>
+      {errorAlert}
+    </div>
+  );
+};
 
 export default SpendingFormView;
